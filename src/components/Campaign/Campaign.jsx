cd0e7a0196b4b57504a5c45379798e66c47b680d
Saye0rle <109271157+Saye0rle@users.noreply.github.com>
2022-12-05 13:20:32 +0300

    up

--- src/components/Campaign/Campaign.jsx
+++ src/components/Campaign/Campaign.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Campaign() {
   return (
-    <div className="h-screen relative bg mt-6 grid place-items-center">
+    <div className="h-screen max-h-[50rem] relative bg mt-6 grid place-items-center">
       <div className="flex flex-col items-center text-white gap-y-3">
         <h2 className="text-[24px] font-bold">New Season Sale</h2>
         <strong className="text-[54px]">40% OFF</strong>
         <span className="bg-white h-1 w-10 "></span>
         <a
-          href=""
+          href="s"
           className="px-8 py-3 bg-white text-black hover:bg-[#1357fe] hover:text-white font-semibold"
         >
           SHOP NOW
