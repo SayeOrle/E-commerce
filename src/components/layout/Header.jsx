deeb7832b9d696f3fb62e3986645bac78383b132
Saye0rle <109271157+Saye0rle@users.noreply.github.com>
2022-12-22 14:49:35 +0300

    up

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -8,8 +8,22 @@ import {
 import { BsSearch, BsBag } from "react-icons/bs";
 import logo from "../../images/mega-menu.jpg";
 import { mainContext, useContext } from "../../context";
+import { useState, useRef, useEffect } from "react";
+
 function Header() {
   const { setModal } = useContext(mainContext);
+  const [sideShow, setSideShow] = useState(false);
+  const sidebarRef = useRef();
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (!sidebarRef.current.contains(e.target)) {
+        e.preventDefault();
+        setSideShow(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+  });
 
   return (
     <div className="fixed bg-white top-0 left-0 w-full z-40">
@@ -22,20 +36,41 @@ function Header() {
           </span>
         </p>
       </div>
-      <div className="h-20 sm:h-24 flex items-center border-b-2">
-        <div className="flex items-center justify-between  container">
+      <div className="h-20 sm:h-24 flex items-center border-b-2 ">
+        <div className="flex items-center justify-between  container ">
+          <button
+            className="sm:hidden block"
+            onClick={() => setSideShow(!sideShow)}
+          >
+            <AiOutlineMenu size={"30px"} />
+          </button>
+
           <div className="font-semibold text-3xl">LOGO</div>
-          <nav className="hidden sm:block font-semibold">
-            <ul className="flex gap-x-7 text-sm ">
-              <li className="h-20 flex items-center relative">
+          <nav
+            ref={sidebarRef}
+            className={`${
+              sideShow ? " left-0  " : " -left-full "
+            }transition-all absolute top-0 w-1/3 h-screen z-10 sm:h-auto  flex flex-col items-center pt-20 sm:p-0  sm:static sm:bg-transparent bg-white  font-semibold`}
+          >
+            <div
+              className="absolute top-4 right-4 cursor-pointer block sm:hidden"
+              onClick={() => setSideShow(false)}
+            >
+              &#x2715;
+            </div>
+            <ul className="flex gap-x-7 sm:gap-y-0 gap-11 text-sm flex-col sm:flex-row ">
+              <li className="sm:h-20 flex items-center relative  ">
                 <a
                   className="tracking-wider flex items-center h-full	peer before:content-[''] before:h-[1px] before:duration-300 before:w-full before:absolute before:scale-x-0 before:hover:scale-x-100 before:bg-[#1367fe] cursor-pointer relative before:bottom-1/2    before:translate-y-3 before:transition-all "
                   href="/"
                 >
                   HOME
                 </a>
-                <AiOutlineDown size={"16px"} className="mt-1 opacity-40" />
-                <div className="absolute w-40 top-full -translate-x-10 translate-y-[2px] justify-center opacity-0 hover:opacity-100 peer-hover:opacity-100 transition-all duration-300 invisible hover:!visible peer-hover:visible  bg-white p-3 border border-gray-300">
+                <AiOutlineDown
+                  size={"16px"}
+                  className="mt-1 opacity-40 hidden sm:block"
+                />
+                <div className="hidden sm:block absolute w-40 top-full -translate-x-10 translate-y-[2px] justify-center opacity-0 hover:opacity-100 peer-hover:opacity-100 transition-all duration-300 invisible hover:!visible peer-hover:visible  bg-white p-3 border border-gray-300">
                   <ul className="w-full flex flex-col  gap-y-1 text-sm px-4">
                     <li>Home clean</li>
                     <li>Home clean</li>
@@ -48,15 +83,18 @@ function Header() {
                   </ul>
                 </div>
               </li>
-              <li className="h-20   flex items-center relative ">
+              <li className="sm:h-20   flex items-center relative ">
                 <a
                   className="tracking-wider flex items-center h-full	peer before:content-[''] before:h-[1px] before:duration-300 before:w-full before:absolute before:scale-x-0 before:hover:scale-x-100 before:bg-[#1367fe] cursor-pointer relative before:bottom-1/2    before:translate-y-3 before:transition-all "
                   href="/shop"
                 >
                   SHOP
                 </a>
-                <AiOutlineDown size={"16px"} className="mt-1 opacity-40" />
-                <div className="absolute invisible opacity-0	flex hover:opacity-100 hover:!visible p-5 h-[27rem] w-[50rem] justify-between top-full -translate-x-1/2 translate-y-[2px]   peer-hover:visible peer-hover:opacity-100 duration-300 transition-all bg-white  border border-gray-300">
+                <AiOutlineDown
+                  size={"16px"}
+                  className="mt-1 opacity-40 hidden sm:block"
+                />
+                <div className="hidden absolute invisible opacity-0	sm:flex hover:opacity-100 hover:!visible p-5 h-[27rem] w-[50rem] justify-between top-full -translate-x-1/2 translate-y-[2px]   peer-hover:visible peer-hover:opacity-100 duration-300 transition-all bg-white  border border-gray-300">
                   <div className="flex justify-between gap-x-14   ">
                     <div className=" ">
                       <h3>Shop Style</h3>
@@ -99,7 +137,7 @@ function Header() {
                   </div>
                 </div>
               </li>
-              <li className="h-20 flex items-center">
+              <li className="sm:h-20 flex items-center">
                 <a
                   className="tracking-wider	 before:content-[''] before:h-[1px] before:duration-300 before:w-full before:absolute before:scale-x-0 before:hover:scale-x-100 before:bg-[#1367fe] cursor-pointer relative before:bottom-0 before:transition-all"
                   href="/blog"
@@ -107,7 +145,7 @@ function Header() {
                   BLOG
                 </a>
               </li>
-              <li className="h-20 flex items-center">
+              <li className="sm:h-20 flex items-center">
                 <a
                   className="tracking-wider	 before:content-[''] before:h-[1px] before:duration-300 before:w-full before:absolute before:scale-x-0 before:hover:scale-x-100 before:bg-[#1367fe] cursor-pointer relative before:bottom-0 before:transition-all"
                   href="/contact"
@@ -118,12 +156,8 @@ function Header() {
             </ul>
           </nav>
           <div className="flex items-center gap-x-2">
-            <button className="sm:hidden block">
-              <AiOutlineMenu />
-            </button>
             <a href="/register">
-            <AiOutlineUser size={"20px"} />
-
+              <AiOutlineUser size={"20px"} />
             </a>
             <button
               onClick={() => {
@@ -134,8 +168,14 @@ function Header() {
             </button>
             <AiOutlineHeart size={"20px"} />
             <div className="relative">
-              <div className="bg-[#1367fe] -z-10  rounded-full flex items-center justify-center text-[10px] text-white h-4 w-4 absolute -top-3 -right-3">1</div>
-              <button className="flex items-center"><a href="/cart"><BsBag /></a></button>
+              <div className="bg-[#1367fe] -z-10  rounded-full flex items-center justify-center text-[10px] text-white h-4 w-4 absolute -top-3 -right-3">
+                1
+              </div>
+              <button className="flex items-center">
+                <a href="/cart">
+                  <BsBag />
+                </a>
+              </button>
             </div>
           </div>
         </div>
--- src/components/ui/Carousel.jsx
+++ src/components/ui/Carousel.jsx
@@ -1,23 +1,36 @@
 import React from "react";
 import sliderImage from "../../images/slider1.jpg";
+import sliderImage2 from "../../images/slider2.jpg";
+import sliderImage3 from "../../images/slider3.jpg";
+
 import { SlArrowLeft } from "react-icons/sl";
 import { SlArrowRight } from "react-icons/sl";
+import Slider from "react-slick";
 
 function Carousel() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="h-screen w-full  pt-[123.99px] sm:pt-[139.98px] ">
-      <div className=" absolute w-full h-[calc(100vh_-_139.98px)] -z-50">
-        <img className="object-cover w-full h-full" src={sliderImage} alt="" />
-      </div>
-      <div className=" h-[calc(100vh_-_123.99px)] sm:h-[calc(100vh_-_139.98px)] w-full flex items-center relative ">
-        <div className="flex justify-between w-full px-5">
-          <button className="rounded-full h-12 w-12 md:h-14 md:w-14 lg:h-16 lg:w-16 text-base md:text-xl lg:text-2xl grid place-items-center bg-white opacity-60 hover:opacity-100 hover:text-white hover:bg-[#1367fe] border-gray-300 border-2 outline-none transition-all">
-            <SlArrowLeft/>
-          </button>
-          <div className="flex-1 flex flex-col gap-y-3 items-start container">
+      <Slider {...settings}>
+        <div className="relative outline-none">
+          <img
+            className="object-cover w-full h-full "
+            src={sliderImage}
+            alt=""
+          />
+          <div className="flex flex-col gap-y-3 items-start container absolute z-50 top-24 left-28">
             <div>
               <p className="text-lg font-medium">SUMMER 2022</p>
-              <h2 className=" text-base sm:text-xl md:text-4xl lg:text-6xl font-semibold">Save up to 70%</h2>
+              <h2 className=" text-base sm:text-xl md:text-4xl lg:text-6xl font-semibold">
+                Save up to 70%
+              </h2>
             </div>
             <a
               href="#s"
@@ -26,16 +39,51 @@ function Carousel() {
               Explore Now
             </a>
           </div>
-          <button className="rounded-full h-12 w-12 md:h-14 md:w-14 lg:h-16 lg:w-16 text-base md:text-xl lg:text-2xl grid place-items-center bg-white opacity-60 hover:opacity-100 hover:text-white hover:bg-[#1367fe] border-gray-300 border-2 outline-none transition-all">
-            <SlArrowRight/>
-          </button>
         </div>
-        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 flex gap-x-2 pb-5">
-          <button className="opacity-60 bg-[#1367fe] hover:opacity-100 rounded-full h-4 w-4"></button>
-          <button className="opacity-60 bg-[#1367fe] hover:opacity-100 rounded-full h-4 w-4"></button>
-          <button className="opacity-60 bg-[#1367fe] hover:opacity-100 rounded-full h-4 w-4"></button>
+        <div className="relative outline-none">
+          <img
+            className="object-cover w-full h-full "
+            src={sliderImage2}
+            alt=""
+          />
+          <div className="flex flex-col gap-y-3 items-start container absolute z-50 top-24 left-28">
+            <div>
+              <p className="text-lg font-medium">SUMMER 2022</p>
+              <h2 className=" text-base sm:text-xl md:text-4xl lg:text-6xl font-semibold">
+                Save up to 70%
+              </h2>
+            </div>
+            <a
+              href="#s"
+              className="px-4 py-2 md:px-5 md:py-3 bg-[#1367fe] text-white font-medium"
+            >
+              Explore Now
+            </a>
+          </div>
         </div>
-      </div>
+        <div className="relative outline-none">
+          <img
+            className="object-cover w-full h-full "
+            src={sliderImage3}
+            alt=""
+          />
+          <div className="flex flex-col gap-y-3 items-start container absolute z-50 top-24 left-28">
+            <div>
+              <p className="text-lg font-medium">SUMMER 2022</p>
+              <h2 className=" text-base sm:text-xl md:text-4xl lg:text-6xl font-semibold">
+                Save up to 70%
+              </h2>
+            </div>
+            <a
+              href="#s"
+              className="px-4 py-2 md:px-5 md:py-3 bg-[#1367fe] text-white font-medium"
+            >
+              Explore Now
+            </a>
+          </div>
+        </div>
+
+      </Slider>
     </div>
   );
 }
